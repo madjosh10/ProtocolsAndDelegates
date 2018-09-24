7555a4a2260eca7443e5ede036f524cc9639925e
madjosh10 <madrigaljosh10@yahoo.com>
2018-09-24 11:25:46 -0500

    Initial Commit

---
+++ ProtocolsAndDelegates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolsAndDelegates
+//
+//  Created by Joshua Madrigal on 9/24/18.
+//  Copyright © 2018 Joshua Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
